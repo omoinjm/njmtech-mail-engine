1daece5709822c4dd645b2cded6cdba6ed9126e8
omoinjm <njmcloud@gmail.com>
2025-06-24 11:39:23 +0200

    ensure each cycle finishes completely before the next one starts

--- src/Mail.Engine.Service.Api/Services/RecurringTaskService.cs
+++ src/Mail.Engine.Service.Api/Services/RecurringTaskService.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Text.Json;
 using Mail.Engine.Service.Application.Queries;
 using MediatR;
@@ -15,6 +16,8 @@ namespace Mail.Engine.Service.Api.Services
 
             while (!stoppingToken.IsCancellationRequested)
             {
+                var stopwatch = Stopwatch.StartNew();
+
                 try
                 {
                     using var scope = _serviceScopeFactory.CreateScope();
@@ -27,21 +30,27 @@ namespace Mail.Engine.Service.Api.Services
                     // Call Wati query
                     var watiResult = await mediator.Send(new GetWatiQuery(), stoppingToken);
 
-                    // var customerLogoutResult = await mediator.Send(new GetCustomerLogoutQuery(), stoppingToken);
+                    var customerLogoutResult = await mediator.Send(new GetCustomerLogoutQuery(), stoppingToken);
 
                     _logger.LogInformation($"Inbound Mails Processed: {JsonSerializer.Serialize(inboundResult)}");
                     _logger.LogInformation($"Outbound Mails Processed: {JsonSerializer.Serialize(outboundResult)}");
-
                     _logger.LogInformation($"Wati Mails Processed: {JsonSerializer.Serialize(watiResult)}");
-
-                    // _logger.LogInformation($"Wati Customer Autologout: {JsonSerializer.Serialize(customerLogoutResult)}");
+                    _logger.LogInformation($"Wati Customer Autologout: {JsonSerializer.Serialize(customerLogoutResult)}");
                 }
                 catch (Exception ex)
                 {
                     _logger.LogError(ex, "Error executing recurring tasks.");
                 }
 
-                await Task.Delay(5000, stoppingToken); // Run every second
+                stopwatch.Stop();
+
+                var timeTaken = stopwatch.ElapsedMilliseconds;
+                var delay = Math.Max(0, 5000 - (int)timeTaken);
+
+                if (delay > 0)
+                {
+                    await Task.Delay(delay, stoppingToken);
+                }
             }
         }
     }
